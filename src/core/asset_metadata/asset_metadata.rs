40eaf3529157a4f2856c4554d2e5448b40f46977
TabooSun <taboosun1996@gmail.com>
2024-04-17 18:01:23 +0800

    Add parse_pubspec_yaml_assets

---
+++ src/core/asset_metadata/asset_metadata.rs
@@ -0,0 +1,124 @@
+use std::path::PathBuf;
+use recase::ReCase;
+
+use crate::core::flutter::pubspec_yaml::FlutterGenAssetsOutputsStyle;
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct AssetMetadata {
+    /// The path to the asset.
+    asset_path: PathBuf,
+    /// The exact configuration from the `pubspec.yaml` file.
+    located_directory: PathBuf,
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum AssetMetadataError {
+    #[error("Failed to retrieve the file name from {path}.")]
+    RetrieveFileStemError {
+        path: PathBuf,
+    },
+}
+
+impl AssetMetadata {
+    pub fn new(
+        asset_path: PathBuf,
+        located_directory: PathBuf,
+    ) -> Self {
+        Self {
+            asset_path,
+            located_directory,
+        }
+    }
+
+    pub fn get_located_directory(&self) -> &PathBuf {
+        &self.located_directory
+    }
+
+    pub fn get_asset_path(&self) -> &PathBuf {
+        &self.asset_path
+    }
+
+    /// Compute a String of the possible usage of the asset in the Dart project.
+    pub fn compute_dart_usage_syntax(&self, flutter_gen_assets_outputs_style: FlutterGenAssetsOutputsStyle) -> Result<String, AssetMetadataError> {
+        let file_stem = self.asset_path.file_stem().ok_or_else(|| AssetMetadataError::RetrieveFileStemError { path: self.asset_path.clone() })?.to_string_lossy().to_string();
+
+        let file_stem_recase = ReCase::new(file_stem);
+
+        // We need to separate the directory name from the file name because ReCase
+        // considers number as a separate word.
+        let relative_asset_path: PathBuf = self.located_directory.components()
+            // `flutter_gen` doesn't support placing assets outside the Flutter recommended
+            // `assets` directory. Plus, `flutter_gen` doesn't include the `assets`
+            // directory in its generated code.
+            .skip(1)
+            .collect();
+        let recase = ReCase::new(relative_asset_path.to_string_lossy().to_string());
+        match flutter_gen_assets_outputs_style {
+            FlutterGenAssetsOutputsStyle::SnakeCase => {
+                let mut result = recase.snake_case();
+                result.push_str("_");
+                result.push_str(&file_stem_recase.snake_case());
+                Ok(result)
+            }
+            FlutterGenAssetsOutputsStyle::CamelCase => {
+                let mut result = recase.camel_case();
+                result.push_str(&file_stem_recase.pascal_case());
+                Ok(result)
+            }
+            FlutterGenAssetsOutputsStyle::DotDelimited => {
+                let mut result = recase.dot_case();
+                result.push_str(".");
+                result.push_str(&file_stem_recase.camel_case());
+                Ok(result)
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn compute_dart_usage_syntax_passes_when_requesting_snake_case() {
+        // Arrange
+        let located_directory = PathBuf::from("assets/images");
+        let asset_path = located_directory.join("image_1.png");
+        let asset_metadata = AssetMetadata::new(asset_path, located_directory);
+
+        // Act
+        let usage_syntax = asset_metadata.compute_dart_usage_syntax(FlutterGenAssetsOutputsStyle::SnakeCase).unwrap();
+
+        // Assert
+        assert_eq!(usage_syntax, "images_image_1");
+    }
+
+    #[test]
+    fn compute_dart_usage_syntax_passes_when_requesting_camel_case() {
+        // Arrange
+        let located_directory = PathBuf::from("assets/images");
+        let asset_path = located_directory.join("image_1.png");
+        let asset_metadata = AssetMetadata::new(asset_path, located_directory);
+
+        // Act
+        let usage_syntax = asset_metadata.compute_dart_usage_syntax(FlutterGenAssetsOutputsStyle::CamelCase).unwrap();
+
+        // Assert
+        assert_eq!(usage_syntax, "imagesImage1");
+    }
+
+    #[test]
+    fn compute_dart_usage_syntax_passes_when_requesting_dot_delimited() {
+        // Arrange
+        let located_directory = PathBuf::from("assets/images");
+        let asset_path = located_directory.join("image_1.png");
+        let asset_metadata = AssetMetadata::new(asset_path, located_directory);
+
+        // Act
+        let usage_syntax = asset_metadata.compute_dart_usage_syntax(FlutterGenAssetsOutputsStyle::DotDelimited).unwrap();
+
+        // Assert
+        assert_eq!(usage_syntax, "images.image1");
+    }
+}
+
--- src/core/asset_metadata/mod.rs
+++ src/core/asset_metadata/mod.rs
@@ -1,99 +1,97 @@
 use std::path::PathBuf;
-use recase::ReCase;
 
-use crate::core::flutter::pubspec_yaml::FlutterGenAssetsOutputsStyle;
+use crate::core::asset_metadata::asset_metadata::AssetMetadata;
 
-pub struct AssetMetadata {
-    asset_path: PathBuf,
-    located_directory: PathBuf,
-}
+pub mod asset_metadata;
 
 #[derive(Debug, thiserror::Error)]
-pub enum AssetMetadataError {
-    /*#[error("The path: {path} is not an absolute path.")]
-    NotAbsolutePathError {
-        path: PathBuf
-    },*/
-    #[error("Failed to retrieve the file name from {path}.")]
-    RetrieveFileStemError {
+pub enum ParsePubspecYamlAssetsError {
+    #[error("Failed to read directory {path}. {source}")]
+    ReadDirectoryError {
         path: PathBuf,
+        source: std::io::Error,
+    },
+    #[error("Failed to fetch directory entry. {source}")]
+    FetchDirectoryEntryError {
+        source: std::io::Error,
     },
 }
 
-impl AssetMetadata {
-    pub fn new(
-        asset_path: PathBuf,
-        located_directory: PathBuf,
-    ) -> Self {
-        /*fn ensure_absolute_path(path: PathBuf) -> Result<PathBuf,AssetMetadataError >{
-            if path.is_absolute() {
-                Ok(path)
-            } else {
-                Err(AssetMetadataError::NotAbsolutePathError{
-                    path
-                })
+pub fn parse_pubspec_yaml_assets(
+    flutter_project_path: &PathBuf,
+    assets: Vec<String>,
+) -> Result<Vec<AssetMetadata>, ParsePubspecYamlAssetsError> {
+    let mut asset_metadatum = Vec::new();
+    for configured_asset in assets {
+        let located_directory = flutter_project_path.join(&configured_asset);
+        for dir_entry in (&located_directory).read_dir().map_err(
+            |err| ParsePubspecYamlAssetsError::ReadDirectoryError {
+                path: located_directory.to_owned(),
+                source: err,
+            },
+        )? {
+            let entry = dir_entry.map_err(
+                |err| ParsePubspecYamlAssetsError::FetchDirectoryEntryError { source: err },
+            )?;
+            let path = entry.path();
+            if !path.is_file() {
+                continue;
             }
-        }
-*/
-        Self {
-            asset_path,
-            located_directory,
-        }
-    }
-    pub fn get_located_directory(&self) -> &PathBuf {
-        &self.located_directory
+            asset_metadatum.push(AssetMetadata::new(
+                path.to_owned(),
+                PathBuf::from(configured_asset.to_owned()).to_owned(),
+            ));
+        };
     }
 
-    pub fn get_asset_path(&self) -> &PathBuf {
-        &self.asset_path
-    }
-
-    /// Compute a String of the possible usage of the asset in the Dart project.
-    pub fn compute_dart_usage_syntax(&self, flutter_gen_assets_outputs_style: FlutterGenAssetsOutputsStyle) -> Result<String, AssetMetadataError> {
-        let file_stem = self.asset_path.file_stem().ok_or_else(|| AssetMetadataError::RetrieveFileStemError { path: self.asset_path.clone() })?.to_string_lossy().to_string();
-        let relative_asset_path = self.located_directory.join(file_stem);
-        let relative_asset_path: PathBuf = relative_asset_path.components()
-            // `flutter_gen` doesn't support placing assets outside the Flutter recommended 
-            // `assets` directory. Plus, `flutter_gen` doesn't include the `assets` 
-            // directory in its generated code.
-            .skip(1)
-            .collect();
-        let recase = ReCase::new(relative_asset_path.to_string_lossy().to_string());
-        match flutter_gen_assets_outputs_style {
-            FlutterGenAssetsOutputsStyle::SnakeCase => {
-                /*let mut usage_syntax: String = String::new();
-                for component in file_components {
-                    usage_syntax.push_str(&component.to_string_lossy());
-                    usage_syntax.push_str("_");
-                }
-                usage_syntax.push_str(&file_stem);*/
-                Ok(recase.snake_case())
-            }
-            FlutterGenAssetsOutputsStyle::CamelCase => {
-                Ok(recase.camel_case())
-            }
-            FlutterGenAssetsOutputsStyle::DotDelimited => {
-                Ok(recase.dot_case())
-            }
-        }
-    }
+    Ok(asset_metadatum)
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::core::flutter::pubspec_yaml;
+    use crate::core::testing_util::artifact::get_test_artifact_directory;
+
     use super::*;
-    
+
     #[test]
-    fn compute_dart_usage_syntax_passes_when_requesting_snake_case() {
+    fn parse_pubspec_yaml_assets_can_work() {
         // Arrange
-        let located_directory = PathBuf::from("assets/images");
-        let asset_path = located_directory.join("image_1.png");
-        let asset_metadata = AssetMetadata::new(asset_path, located_directory);
-        
+        let test_artifact_directory = get_test_artifact_directory().unwrap();
+        let unique_id = uuid::Uuid::new_v4();
+        let test_artifact_directory = test_artifact_directory.join(unique_id.to_string());
+        std::fs::create_dir_all(&test_artifact_directory).unwrap();
+        fs_extra::dir::copy(
+            std::env::current_dir().unwrap().join("tests/sample/flutter/dummy"),
+            &test_artifact_directory,
+            &fs_extra::dir::CopyOptions::new(),
+        ).unwrap();
+        let flutter_project_path = test_artifact_directory.join("dummy");
+        let pubspec_yaml = pubspec_yaml::read_pubspec_yaml_file(&flutter_project_path).unwrap();
+
         // Act
-        let usage_syntax = asset_metadata.compute_dart_usage_syntax(FlutterGenAssetsOutputsStyle::SnakeCase).unwrap();
-        
+        let result = parse_pubspec_yaml_assets(
+            &flutter_project_path,
+            pubspec_yaml.flutter.assets,
+        ).unwrap();
+
         // Assert
-        assert_eq!(usage_syntax, "images_image_1");
+        let assets_image_directory = PathBuf::from("assets/images/");
+        assert_eq!(
+            result,
+            vec![
+                AssetMetadata::new(
+                    flutter_project_path.join(&assets_image_directory).join("coming_soon_1.png"),
+                    assets_image_directory.to_owned(),
+                ),
+                AssetMetadata::new(
+                    flutter_project_path.join(&assets_image_directory).join("coming_soon1.png"),
+                    assets_image_directory.to_owned(),
+                ),
+            ]
+        );
+
+        // Cleanup
+        std::fs::remove_dir_all(&test_artifact_directory).unwrap();
     }
 }
